cd010a983541f316bf6ef8fac39216f05e9e4a0a
Andrëw <Francisco.Castillo.Ibarra@gmail.com>
2021-09-15 18:23:21 -0300

    Fixed the last commit because i as dumb.

--- pages/components/Results/ResultsHome.js
+++ pages/components/Results/ResultsHome.js
@@ -28,7 +28,7 @@ export default function ResultsHome({ imageUrls, setShowImages }) {
                 <div className="row align-items-top">
                     {imageUrls.map((image, key) => {
                         return (
-                            <div className="col-sm-6">
+                            <div className="col-sm-6" key={key}>
                                 <a href={image}>
                                     <Image
                                         src = {image}
